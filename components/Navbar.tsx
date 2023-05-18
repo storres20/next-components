c7c8d04c4d8a040dfc7dd44d5f1a0113e9a55bfd
Italo Lon kan <sanchotorres777@gmail.com>
2023-05-17 22:09:06 -0500

    Fix error messages

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
               <div>
                 <a
                   href="#"
-                  className="flex items-center space-x-1 py-5 px-3 text-gray-700"
+                  className="flex items-center space-x-1 py-5 text-gray-700"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -73,13 +73,13 @@ export default function Navbar() {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   className="w-6 h-6"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                   />
                 </svg>
